489fbf36d0de1b3175c7221867a4eff0ae34bd9e
Cao, Yu <caoyu.work@hotmail.com>
2021-05-01 22:47:40 +0800

    Configure routing for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import {HomeComponent} from './home/home.component';
 import {MonitoringPlanComponent} from './monitoring-plan/monitoring-plan.component';
 import {WaterTemperatureComponent} from './water-temperature/water-temperature.component';
 import {BasicInfoComponent} from './basic-info/basic-info.component';
+import {IceConditionComponent} from './ice-condition/ice-condition.component';
 
 const routes: Routes = [
   {path: 'Home', component: HomeComponent},
   {path: 'BasicInfo', component: BasicInfoComponent},
   {path: 'MonitoringPlan', component: MonitoringPlanComponent},
   {path: 'WaterTemperature', component: WaterTemperatureComponent},
-  {path: 'IceCondition', component: HomeComponent},
+  {path: 'IceCondition', component: IceConditionComponent},
 ];
 
 @NgModule({
