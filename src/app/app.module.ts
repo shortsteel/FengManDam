dd31d601f4fe9e0f603ef6d4cf4fda55cadb0f1c
Cao, Yu <caoyu.work@hotmail.com>
2021-05-01 22:04:22 +0800

    change background image of home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTabsModule} from '@angular/material/tabs';
-import { HomeComponent } from './home/home.component';
-import { BasicInfoComponent } from './basic-info/basic-info.component';
-import { AirTemperatureComponent } from './air-temperature/air-temperature.component';
-import { WaterTemperatureComponent } from './water-temperature/water-temperature.component';
-import { IceConditionComponent } from './ice-condition/ice-condition.component';
+import {MatCardModule} from '@angular/material/card';
+import {HomeComponent} from './home/home.component';
+import {BasicInfoComponent} from './basic-info/basic-info.component';
+import {AirTemperatureComponent} from './air-temperature/air-temperature.component';
+import {WaterTemperatureComponent} from './water-temperature/water-temperature.component';
+import {IceConditionComponent} from './ice-condition/ice-condition.component';
 
 @NgModule({
   declarations: [
@@ -24,9 +25,11 @@ import { IceConditionComponent } from './ice-condition/ice-condition.component';
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatTabsModule
+    MatTabsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
