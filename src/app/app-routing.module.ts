c9c6918093ff8b5f47e3eda3cf3445755b8ca5b7
Cao, Yu <caoyu.work@hotmail.com>
2021-05-02 11:24:27 +0800

    add routing for basic info components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,23 @@ import {MonitoringPlanComponent} from './monitoring-plan/monitoring-plan.compone
 import {WaterTemperatureComponent} from './water-temperature/water-temperature.component';
 import {BasicInfoComponent} from './basic-info/basic-info.component';
 import {IceConditionComponent} from './ice-condition/ice-condition.component';
+import {BasinOverviewComponent} from './basic-info/basin-overview/basin-overview.component';
+import {ProjectOverviewComponent} from './basic-info/project-overview/project-overview.component';
+import {ScheduleOperationComponent} from './basic-info/schedule-operation/schedule-operation.component';
+import {WeatherConditionComponent} from './basic-info/weather-condition/weather-condition.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'Home', pathMatch: 'full'},
   {path: 'Home', component: HomeComponent},
-  {path: 'BasicInfo', component: BasicInfoComponent},
+  {
+    path: 'BasicInfo', component: BasicInfoComponent, children: [
+      {path: '', redirectTo: 'BasinOverview', pathMatch: 'full'},
+      {path: 'BasinOverview', component: BasinOverviewComponent},
+      {path: 'ProjectOverview', component: ProjectOverviewComponent},
+      {path: 'ScheduleOperation', component: ScheduleOperationComponent},
+      {path: 'WeatherCondition', component: WeatherConditionComponent}
+    ]
+  },
   {path: 'MonitoringPlan', component: MonitoringPlanComponent},
   {path: 'WaterTemperature', component: WaterTemperatureComponent},
   {path: 'IceCondition', component: IceConditionComponent},
